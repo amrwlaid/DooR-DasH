278ba70b3b6c80ecd9287fbd1bc39de6a65f86a6
ahmed-53269 <azainhom29@gmail.com>
2026-03-03 06:49:15 +0200

    Refactor Schemer class to include Role import

--- src/game/engine/monsters/Schemer.java
+++ src/game/engine/monsters/Schemer.java
@@ -1,7 +1,9 @@
 // monsters package
 package monsters;
+import game.engine.Role;
 public class Schemer extends Monster{
 public Schemer(String name, String description, Role role, int energy){
 	super( name,  description,  role,  energy);	
 }
 }
+
