fff00e639474a66732334ee05400ec91d2a455e9
ahmed-53269 <azainhom29@gmail.com>
2026-03-03 06:50:00 +0200

    Refactor Dynamo class to include Role import

--- src/game/engine/monsters/Dynamo.java
+++ src/game/engine/monsters/Dynamo.java
@@ -1,7 +1,9 @@
 // monsters package
+import game.engine.Role;
 package monsters;
 public class Dynamo extends Monster{
 public  Dynamo(String name, String description, Role role, int energy){
 	super( name,  description,  role,  energy);
 }
 }
+
