cb4f1c3b7671594f009009d3857511ca59a2a2c0
ahmed-53269 <azainhom29@gmail.com>
2026-03-03 06:50:36 +0200

    Refactor Dynamo class with proper package declaration

--- src/game/engine/monsters/Dynamo.java
+++ src/game/engine/monsters/Dynamo.java
@@ -1,9 +1,11 @@
 // monsters package
-import game.engine.Role;
+
 package monsters;
+import game.engine.Role;
 public class Dynamo extends Monster{
 public  Dynamo(String name, String description, Role role, int energy){
 	super( name,  description,  role,  energy);
 }
 }
+
 
