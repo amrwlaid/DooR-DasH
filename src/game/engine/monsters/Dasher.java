efd64bf847b542e66a151c5b352736bb19d84bbb
ahmed-53269 <azainhom29@gmail.com>
2026-03-03 06:50:13 +0200

    Reformat Dasher class with consistent package declaration

--- src/game/engine/monsters/Dasher.java
+++ src/game/engine/monsters/Dasher.java
@@ -1,5 +1,7 @@
 //monsters package
+
 package monsters;
+import game.engine.Role;
 public class Dasher extends Monster{
 	private int momentumTurns;
 	public  Dasher(String name, String description, Role role, int energy){
@@ -16,3 +18,4 @@ public String toString(){
 	return super.toString() + "\n" + "MomentumTurns : " + momentumTurns;
 }
 }
+
