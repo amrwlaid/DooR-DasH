1ff450939a4c399f7c6ad3b1026380c5ab613ea1
mohamedserag203-byte <mohamedserag203@gmail.com>
2026-03-05 21:13:51 +0200

    Create Cell.java

---
+++ src/game/engine/cells/Cell.java
@@ -0,0 +1,24 @@
+package game.engine.cells;
+
+public class Cell {
+	private String name;
+	private Monster monster;
+	
+	public String getName() {
+		return name;
+	}
+	public Monster getMonster() {
+		return monster;
+	}
+	public void setMonster(Monster monster) {
+		this.monster=monster;
+	}
+	public Cell() {
+		this.monster = null;
+	}
+	public Cell(String name) {
+		this.name = name;
+		this.monster = null;
+	}
+	
+}
