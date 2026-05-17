0d92027604a00734433a7e5c698e04902da7e079
mohamedserag203-byte <mohamedserag203@gmail.com>
2026-05-17 18:23:58 +0300

    Update Game.java

--- src/game/engine/Game.java
+++ src/game/engine/Game.java
@@ -19,6 +19,7 @@ public class Game {
 	
 	public Game(Role playerRole) throws IOException {
 		this.board = new Board(DataLoader.readCards());
+		this.board.initializeBoard(DataLoader.readCells());
 		
 		this.allMonsters = DataLoader.readMonsters();
 		
